3b257f18ce3e177f16ee6bcc5c5fb0ac59d6fd15
Prasad Chavan <prasadchavan7856@gmail.com>
2023-11-14 11:49:48 +0530

    complete project

--- image_transformation.py
+++ image_transformation.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 st.title("Image Transformations WebApp")
-st.markdown("**Developed by Sainath Ingle**", unsafe_allow_html=True)
+st.markdown("**Developed by  Prasad Chavan**", unsafe_allow_html=True)
 
 image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
